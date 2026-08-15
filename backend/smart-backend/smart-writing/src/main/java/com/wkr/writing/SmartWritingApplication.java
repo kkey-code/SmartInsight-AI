be8912cc6e748ecb51ec9f9ddd31fe2d1075e73b
kkey-code <1926877200@qq.com>
2026-08-15 20:57:23 +0800

    feat: add writing service application

---
+++ backend/smart-backend/smart-writing/src/main/java/com/wkr/writing/SmartWritingApplication.java
@@ -0,0 +1,12 @@
+package com.wkr.writing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWritingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWritingApplication.class, args);
+    }
+}
